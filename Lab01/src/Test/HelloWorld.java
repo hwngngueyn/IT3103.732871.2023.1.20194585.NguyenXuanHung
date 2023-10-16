a680cfde2f3bb90f100b848fd296004176c891b5
hwngngueyn <119343865+hwngngueyn@users.noreply.github.com>
2023-10-16 09:30:37 +0700

    Add test1,test2

---
+++ Lab01/src/Test/HelloWorld.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class HelloWorld {
+  public static void main(String [] args ) {
+	  System.out.println("Xin Chao \n cac ban !");
+	  System.out.println("Hello \t world!");
+  }
+}
