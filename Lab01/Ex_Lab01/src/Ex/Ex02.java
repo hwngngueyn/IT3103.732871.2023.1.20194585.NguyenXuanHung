5041b0dd85b8788e2f0c9a18e4dfaf8919380f91
hwngngueyn <119343865+hwngngueyn@users.noreply.github.com>
2023-10-16 10:08:47 +0700

    Add Ex02,triglestar

---
+++ Lab01/Ex_Lab01/src/Ex/Ex02.java
@@ -0,0 +1,17 @@
+package Ex;
+import java.util.Scanner;
+public class Ex02 {
+	public static void main(String args[]) {
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("What's your name?");
+		String strName = keyboard.nextLine();
+		System.out.println("How old are you?");
+		int iAge = keyboard.nextInt();
+		System.out.println("How tall are you (m)?");
+		double dHeight = keyboard.nextDouble();
+		System.out.println("Mr/Ms. " + strName + "," + iAge + " year old. "
+				+"Your height is " + dHeight + ".");
+	}
+
+}
---
+++ Lab01/Ex_Lab01/src/Ex/triglestar.java
@@ -0,0 +1,27 @@
+package Ex;
+import java.util.Scanner;
+
+public class triglestar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the height of the triangle: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces before the stars
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            // Print the stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }
+}
